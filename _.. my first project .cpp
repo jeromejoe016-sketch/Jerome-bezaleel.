2a7993738f7dda1b4ccb6539e85d242ccf53ec16
jeromejoe016-sketch <jeromejoe06.6@gmail.com>
2025-09-19 14:37:51 +0530

    Add files via upload

---
+++ _.. my first project .cpp
@@ -0,0 +1,25 @@
+<textarea name="message" placeholder="Your Message" rows="5" required></textarea>
+
+        <button type="submit" class="btn">Send Message</button>
+
+        <p id="formStatus"></p>
+
+      </form>
+
+    </div>
+
+  </section>
+
+  <!-- Footer -->
+
+  <footer class="footer">
+
+    <p>© <span id="year"></span> jerome bezaleel | Made with 💖</p>
+
+  </footer>
+
+  <script src="script.js"></script>
+
+</body>
+
+</html>
